80e30094a2af2b1b0ed31fea53f0229b23d92d3f
Nikita Hismatov <hismatovns@gmail.com>
2014-12-14 23:57:52 +0500

    врезаем collection_select для главного таксона

--- app/overrides/add_field_to_product_edit_form.rb
+++ app/overrides/add_field_to_product_edit_form.rb
@@ -1,7 +1,12 @@
-#Deface::Override.new(virtual_path:  "backend/app/views/spree/admin/products/form",
+inserting_field =
+"""
+<div class='twelve columns alpha omega'>
+    <label>Главный таксон:</label>
+    <%= collection_select(:product, :main_taxon_id, Spree::Taxon.order(:name), :id, :name, {include_blank: true}) %>
+</div>
+"""
 Deface::Override.new(virtual_path:  "spree/admin/products/_form",
                      name:          "main_taxon_edit_field",
                      insert_before: "[data-hook='admin_product_form_meta']",
-#                    insert_before: "#product_taxons_field",
-                     text:          "<div class='twelve columns alpha omega'>редактируем здесь главный таксон</div>",
+                     erb:      inserting_field,
                      disabled: false)
